1a3de9ec34d31771ba852f2b965c7cec3da15ff4
Tarık Seyceri <tarik@seyceri.info>
2020-09-11 16:21:12 +0300

    Update CleanWindows.bat

--- CleanWindows.bat
+++ CleanWindows.bat
@@ -4,6 +4,7 @@ cls
 
 @echo off
 
+rem Check If User Has Admin Privileges
 NET SESSION >nul 2>&1
 if %ERRORLEVEL% EQU 0 (
     echo Running..
@@ -16,24 +17,28 @@ if %ERRORLEVEL% EQU 0 (
 
 echo.
 
+rem Delete Temporary Files
 del /s /f /q %WinDir%\Temp\*.*
 del /s /f /q %WinDir%\Prefetch\*.*
 del /s /f /q %Temp%\*.*
 del /s /f /q %AppData%\Temp\*.*
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
 
+rem Delete Temporary Folders
 rd /s /q %WinDir%\Temp
 rd /s /q %WinDir%\Prefetch
 rd /s /q %Temp%
 rd /s /q %AppData%\Temp
 rd /s /q %HomePath%\AppData\LocalLow\Temp
 
+rem Recreate Empty Temporary Folders
 md %WinDir%\Temp
 md %WinDir%\Prefetch
 md %Temp%
 md %AppData%\Temp
 md %HomePath%\AppData\LocalLow\Temp
 
+rem Edit Windows.old Folder Privileges and Deleting It
 takeown /F %SYSTEMDRIVE%\Windows.old\* /R /A /D Y
 echo Y | cacls %SYSTEMDRIVE%\Windows.old\*.* /T /grant administrators:F
 echo Please wait, this may take a while.
